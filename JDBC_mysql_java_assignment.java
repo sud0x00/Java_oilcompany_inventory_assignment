cfc5ca7c65d3e65d676d7063a59d38dd62cb6a00
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2022-01-03 11:25:35 +0530

    Update JDBC_mysql_java_assignment.java

--- JDBC_mysql_java_assignment.java
+++ JDBC_mysql_java_assignment.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
  
 public class JDBC_mysql_java_assignment {
-   static final String DB_URL = "jdbc:mysql://localhost/inventory";
+   static final String DB_URL = "jdbc:mysql://localhost/data/inventory";
    static final String USER = "root";
    static final String PASS = "fag4lyf";
  
@@ -15,17 +15,17 @@ public class JDBC_mysql_java_assignment {
          ResultSet rs = x.executeQuery(QUERY);
       ) {             
          System.out.println("Inserting records into the table...");          
-         String sql = "INSERT INTO INVENTORY VALUES (1,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (1,100, 310 , 404 , 124 , 1244 , 201 , 408 )";
          x.executeUpdate(sql);
-         String sql = "INSERT INTO INVENTORY VALUES (2,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (2,200, 290 , 405 , 209 , 4204 , 202 , 405 )";
          x.executeUpdate(sql);
-         String sql = "INSERT INTO INVENTORY VALUES (3,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (3,300, 270 , 406 , 202 , 4214 , 280 , 403 )";
          x.executeUpdate(sql);
-         String sql = "INSERT INTO INVENTORY VALUES (4,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (4,400, 250 , 407 , 209 , 4024 , 260 , 401 )";
          x.executeUpdate(sql);
-         String sql = "INSERT INTO INVENTORY VALUES (5,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (5,500, 230 , 408 , 205 , 1234 , 250 , 402 )";
          x.executeUpdate(sql);
-         String sql = "INSERT INTO INVENTORY VALUES (6,100, 200 , 404 , 200 , 404 , 200 , 404 )";
+         String sql = "INSERT INTO INVENTORY VALUES (6,600, 210 , 409 , 201 , 3504 , 220 , 467 )";
          x.executeUpdate(sql);
          System.out.println("Inserted records into the table...");      
          while(rs.next()){
